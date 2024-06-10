5d45fea4a8e4c91590873b0dbb9c675daac7fc95
jukbuin <h04095@naver.com>
2024-06-10 20:20:38 +0900

    2024/06/10 미들웨어기능 삭제

--- app/write/page.js
+++ app/write/page.js
@@ -2,44 +2,60 @@
 import {useEffect, useState} from "react";
 
 export default function Write() {
+    let [session, setSession] = useState(null);
     let [src, setSrc] = useState('')
 
-    return (
-        <div className="p-20">
-            <h4>글작성</h4>
-            <form action="/api/post/new" method="post">
-                <input type="text" name="title" placeholder="제목을 입력해주세요."/>
-                <textarea name="content" placeholder="내용을 입력해주세요."/>
-                <input type="file" accept="image/*"
-                       onChange={async (e) => {
-                           let file = e.target.files[0]
-                           let filename = encodeURIComponent(file.name)
-                           let res = await fetch(`/api/post/image?file=${filename}`)
-                           res = await res.json()
+    useEffect(() => {
+        fetch('/api/auth/session')
+            .then((r) => {
+                if (r.status === 200) return r.json();
+                return null;
+            })
+            .then((session) => {
+                setSession(session);
+            });
+    }, []);
 
-                           //S3 업로드
-                           const formData = new FormData()
-                           Object.entries({...res.fields, file}).forEach(([key, value]) => {
-                               formData.append(key, value)
-                           })
-                           let uploadResult = await fetch(res.url, {
-                               method: 'POST',
-                               body: formData,
-                           })
-                           console.log(uploadResult)
 
-                           if (uploadResult.ok) {
-                               setSrc(uploadResult.url + '/' + filename)
-                           } else {
-                               console.log('실패')
-                           }
+    if (!session) {
+        return <div>로그인하세요</div>
+    } else {
+        return (
+            <div className="p-20">
+                <h4>글작성</h4>
+                <form action="/api/post/new" method="post">
+                    <input type="text" name="title" placeholder="제목을 입력해주세요."/>
+                    <textarea name="content" placeholder="내용을 입력해주세요."/>
+                    <input type="file" accept="image/*"
+                           onChange={async (e) => {
+                               let file = e.target.files[0]
+                               let filename = encodeURIComponent(file.name)
+                               let res = await fetch(`/api/post/image?file=${filename}`)
+                               res = await res.json()
 
-                       }}/>
-                <img src={src}/>
-                <input type="hidden" name="image_url" value={src}/>
-                <button style={{display: "block"}} type="submit">작성하기</button>
-            </form>
-        </div>
-    )
+                               //S3 업로드
+                               const formData = new FormData()
+                               Object.entries({...res.fields, file}).forEach(([key, value]) => {
+                                   formData.append(key, value)
+                               })
+                               let uploadResult = await fetch(res.url, {
+                                   method: 'POST',
+                                   body: formData,
+                               })
+                               console.log(uploadResult)
 
+                               if (uploadResult.ok) {
+                                   setSrc(uploadResult.url + '/' + filename)
+                               } else {
+                                   console.log('실패')
+                               }
+
+                           }}/>
+                    <img src={src}/>
+                    <input type="hidden" name="image_url" value={src}/>
+                    <button style={{display : "block"}} type="submit">작성하기</button>
+                </form>
+            </div>
+        )
+    }
 }
--- middleware.js
+++ middleware.js
@@ -2,11 +2,11 @@ import {NextResponse} from "next/server";
 import {getToken} from "next-auth/jwt";
 
 export async function middleware(req) {
-    const session = await getToken({req : req})
-    if(req.nextUrl.pathname.startsWith('/write')) {
-        if (session == null) {
-            return NextResponse.redirect('http://mukkitlist-env.eba-awgiwhjf.ap-northeast-2.elasticbeanstalk.com/api/auth/signin')
-        }
-    }
+    // const session = await getToken({req : req})
+    // if(req.nextUrl.pathname.startsWith('/write')) {
+    //     if (session == null) {
+    //         return NextResponse.redirect('http://mukkitlist-env.eba-awgiwhjf.ap-northeast-2.elasticbeanstalk.com/api/auth/signin')
+    //     }
+    // }
 
 }
